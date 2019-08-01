2748c7c26cb5190551b2f7dfad369cb64837ea1a
Naeim Semsarilar <nsemsarilar@box.com>
2019-07-31 20:01:44 -0400

    More cleanup and things

--- classifier/cbct/prepare_png_files.py
+++ classifier/cbct/prepare_png_files.py
@@ -11,9 +11,10 @@ from natsort import natsorted
 sourceDir = '../../_data'
 dcmStudiesDir = os.path.join(sourceDir, 'dcm')
 pngStudiesDir = os.path.join(sourceDir, 'png')
+pngColorStudiesDir = os.path.join(sourceDir, 'png_color')
 jpgStudiesDir = os.path.join(sourceDir, 'jpg')
 
-def convertDCMtoPNGandJPG(dcmPath, pngPath, jpgPath):
+def convertDCMtoPNGandJPG(dcmPath, pngPath, pngColorPath, jpgPath):
     ds = pydicom.dcmread(dcmPath)
 
     shape = ds.pixel_array.shape
@@ -34,11 +35,17 @@ def convertDCMtoPNGandJPG(dcmPath, pngPath, jpgPath):
 
     # Convert to uint
     image_2d_scaled = np.uint8(image_2d_scaled)
+    image_2d_color = np.repeat(image_2d_scaled, 3, axis=1)
 
     # Write the PNG file
     with open(pngPath, 'wb') as png_file:
         w = png.Writer(shape[1], shape[0], greyscale=True)
         w.write(png_file, image_2d_scaled)
+
+    # Write the PNG color file
+    with open(pngColorPath, 'wb') as png_color_file:
+        w = png.Writer(shape[1], shape[0], greyscale=False)
+        w.write(png_color_file, image_2d_color)
 
     # Write JPG file
     im = Image.fromarray(image_2d_scaled)
@@ -50,6 +57,10 @@ print('deleting ' + pngStudiesDir)
 shutil.rmtree(pngStudiesDir, ignore_errors=True)
 os.makedirs(pngStudiesDir)
 
+print('deleting ' + pngColorStudiesDir)
+shutil.rmtree(pngColorStudiesDir, ignore_errors=True)
+os.makedirs(pngColorStudiesDir)
+
 print('deleting ' + jpgStudiesDir)
 shutil.rmtree(jpgStudiesDir, ignore_errors=True)
 os.makedirs(jpgStudiesDir)
@@ -58,6 +69,7 @@ os.makedirs(jpgStudiesDir)
 for root, dirNames, fileNames in os.walk(dcmStudiesDir):
     for dirName in dirNames:
         os.makedirs(os.path.join(pngStudiesDir, dirName), exist_ok=True)
+        os.makedirs(os.path.join(pngColorStudiesDir, dirName), exist_ok=True)
         os.makedirs(os.path.join(jpgStudiesDir, dirName), exist_ok=True)
         for root, dirNames, fileNames in os.walk(os.path.join(dcmStudiesDir, dirName)):
             for fileName in natsorted(fileNames):
@@ -65,7 +77,8 @@ for root, dirNames, fileNames in os.walk(dcmStudiesDir):
                     dcmPath = os.path.join(dcmStudiesDir, dirName, fileName)
                     number = re.findall(r'\d+', fileName)[0]
                     pngPath = os.path.join(pngStudiesDir, dirName, dirName + '_' + number + '.png')
+                    pngColorPath = os.path.join(pngColorStudiesDir, dirName, dirName + '_' + number + '.png')
                     jpgPath = os.path.join(jpgStudiesDir, dirName, dirName + '_' + number + '.jpg')
                     print('converting ' + dcmPath)
-                    convertDCMtoPNGandJPG(dcmPath, pngPath, jpgPath)
+                    convertDCMtoPNGandJPG(dcmPath, pngPath, pngColorPath, jpgPath)
     break
