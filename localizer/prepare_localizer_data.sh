81a361c92428f106b54352b8882d10c84bb08431
Naeim Semsarilar <naeims@gmail.com>
2019-11-05 23:55:38 -0500

    Cleaning up and adding documentation

---
+++ localizer/prepare_localizer_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Load mapping.csv
+# Make dir data/training
+# For every study that is in the training set
+#    Copy each file from _data/png into _data/localizer_training, e.g. 10_49.png
+#    Copy labels from _data/labels_raw into _data/localizer_training, e.g. 10_49_mask.png
+#       --- convert greyscale+red to black+white
+#       --- for every non-black label, put a copy in data/labels
+
+python ./prepare_localizer_data.py
